dfcf9d67faa80c5cd00a006572ff76f98e37cf09
Yago-Caetano <46303870+Yago-Caetano@users.noreply.github.com>
2021-08-16 22:19:26 -0300

    Compromisso Model Implementado

---
+++ src/com/agenda/models/CompromissoModel.java
@@ -0,0 +1,49 @@
+package com.agenda.models;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CompromissoModel {
+        private String Titulo;
+
+        private Calendar Data;
+
+        private String Descricao;
+
+        private Date DataAviso;
+
+        public CompromissoModel() {
+        }
+
+        public String getTitulo() {
+                return Titulo;
+        }
+
+        public void setTitulo(String titulo) {
+                Titulo = titulo;
+        }
+
+        public Calendar getData() {
+                return Data;
+        }
+
+        public void setData(Calendar data) {
+                Data = data;
+        }
+
+        public String getDescricao() {
+                return Descricao;
+        }
+
+        public void setDescricao(String descricao) {
+                Descricao = descricao;
+        }
+
+        public Date getDataAviso() {
+                return DataAviso;
+        }
+
+        public void setDataAviso(Date dataAviso) {
+                DataAviso = dataAviso;
+        }
+}
